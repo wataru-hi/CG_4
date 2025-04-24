ba49c23d5f4e2df369df53e86a1553572016e24a
wataru-hi <146914543+wataru-hi@users.noreply.github.com>
2025-04-24 11:38:45 +0900

    a

--- DirectXGame/GameScene.cpp
+++ DirectXGame/GameScene.cpp
@@ -8,24 +8,32 @@ using namespace MathUtility;
 std::random_device seedGenerator;
 std::mt19937 randomEngine(seedGenerator());
 std::uniform_real_distribution<float> distridution(-1.0f, 1.0f);
+std::uniform_real_distribution<float> radianFromZero(0, 2.0f * PI);
+std::uniform_real_distribution<float> radianFromMinus2pi(-2.0f * PI, 2.0f * PI);
 
 GameScene::~GameScene() {
 	delete modelEfect_;
 }
 
 void GameScene::Initialize() {
+	srand((unsigned)time(NULL));
+	
 	modelEfect_ = Model::CreateFromOBJ("board");
 
 	worldTransform.Initialize();
 
+	worldTransform.scale_.y = randomEngine(radianFromZero)
+
 	camera_.Initialize();
 
-	//srand((unsigned)time(NULL));
+	
 	
 }
 
 void GameScene::Update() {
 	
+
+
 	ImGui::Begin("board");
 	ImGui::DragFloat3("rot", &worldTransform.rotation_.x, 0.1f);
 	ImGui::DragFloat3("sca", &worldTransform.scale_.x, 0.1f);
